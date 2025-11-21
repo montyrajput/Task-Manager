0dddbb841253925051d9e529ee4624f3182fcf21
montyrajput <montysinghrajput590@gmail.com>
2025-11-21 18:49:42 +0530

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-// backend/server.js
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -6,30 +6,30 @@ const path = require("path");
 
 const app = express();
 
-// Import Routes
+
 const authRoutes = require("./routes/auth");
 const taskRoutes = require("./routes/tasks");
 
-// Initialize Database (runs table creation)
+
 require("./config/db");
 
-// --- MIDDLEWARES ---
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // your React (Vite) frontend URL
+    origin: "https://task-manager-iota-rosy.vercel.app/", 
     credentials: true,
   })
 );
 
 app.use(express.json());
 
-// --- ROUTES ---
-app.use("/api/auth", authRoutes); // /api/auth/register, /api/auth/login
-app.use("/api/tasks", taskRoutes); // /api/tasks/...
 
-// Health check
+app.use("/api/auth", authRoutes); 
+app.use("/api/tasks", taskRoutes); 
+
+
 app.get("/health", (req, res) => res.json({ ok: true }));
 
-// --- SERVER START ---
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
